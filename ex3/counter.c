9e209136a5c287c8968ca48eed20ae6cba1e8449
Dor Duchovni <dor@falcore.co>
2017-05-13 18:38:55 +0300

    formatting

--- ex3/counter.c
+++ ex3/counter.c
@@ -1,95 +1,85 @@
-#include <stdio.h>
-#include <memory.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <errno.h>
-#include <unistd.h>
-#include <sys/stat.h>
+#include <fcntl.h>
+#include <memory.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{
-    bool finished = false;
-    if (argc != 5)
-    {
-        printf("Invalid args\n");
-        return -1;
-    }
-    size_t cnt = 0;
-    char targetChar = argv[1][0];
-    char fileName[256];
-    char pipeName[256] = "/tmp/counter_";
-    strcpy(fileName, argv[2]);
-    off_t offset;
-    sscanf(argv[3], "%jd", &offset);
-    size_t length;
-    sscanf(argv[4], "%zu", &length);
-    if (length == 0)
-    {
-        printf("empty file\n");
-        exit(0);
-    }
-    int fd = open(fileName, O_RDONLY);
-
-    // CREDIT mmap: recitation 5
-    char *arr = (char *)mmap(NULL, length, PROT_READ, MAP_SHARED, fd, offset);
-    if (arr == MAP_FAILED)
-    {
-        printf("Error mmapping the file: %s\n", strerror(errno));
-        return -1;
-    }
-    size_t i;
-    for (i = 0; i < length; i++)
-    {
-        if (arr[i] == targetChar)
-        {
-            cnt++;
-        }
-    }
-
-    pid_t myPid = getpid();
-    sprintf(pipeName, "%s%d", pipeName, myPid);
-    mkfifo(pipeName, 0666);
-    void signalHandler(int signum, siginfo_t *info, void *ptr)
-    {
-        pid_t pid = info->si_pid;
-        if (pid != 0)
-        {
-            int outFd = open(pipeName, O_WRONLY);
-            write(outFd, &cnt, sizeof(cnt));
-            sleep(1);
-            if (munmap(arr, length) == -1)
-            {
-                printf("Error un-mmapping the file: %s\n", strerror(errno));
-                exit(0);
-            }
-            close(fd);
-            close(outFd);
-            unlink(pipeName);
-            finished = true;
-            exit(0);
-        }
-        return;
-    }
-    struct sigaction new_action;
-    memset(&new_action, 0, sizeof(new_action));
-    new_action.sa_sigaction = signalHandler;
-    new_action.sa_flags = SA_SIGINFO;
-    if (0 != sigaction(SIGUSR2, &new_action, NULL))
-    {
-        printf("Signal handle registration "
-               "failed. %s\n",
-               strerror(errno));
-        return -1;
-    }
-
-    pid_t dispatcherPid = getppid();
-
-    while (!finished)
-    {
-        int killStat = kill(dispatcherPid, SIGUSR1);
-    }
+int main(int argc, char **argv) {
+  bool finished = false;
+  if (argc != 5) {
+    printf("Invalid args\n");
+    return -1;
+  }
+  size_t cnt = 0;
+  char targetChar = argv[1][0];
+  char fileName[256];
+  char pipeName[256] = "/tmp/counter_";
+  strcpy(fileName, argv[2]);
+  off_t offset;
+  sscanf(argv[3], "%jd", &offset);
+  size_t length;
+  sscanf(argv[4], "%zu", &length);
+  if (length == 0) {
+    printf("empty file\n");
     exit(0);
+  }
+  int fd = open(fileName, O_RDONLY);
+
+  // CREDIT mmap: recitation 5
+  char *arr = (char *)mmap(NULL, length, PROT_READ, MAP_SHARED, fd, offset);
+  if (arr == MAP_FAILED) {
+    printf("Error mmapping the file: %s\n", strerror(errno));
+    return -1;
+  }
+  size_t i;
+  for (i = 0; i < length; i++) {
+    if (arr[i] == targetChar) {
+      cnt++;
+    }
+  }
+
+  pid_t myPid = getpid();
+  sprintf(pipeName, "%s%d", pipeName, myPid);
+  mkfifo(pipeName, 0666);
+  void signalHandler(int signum, siginfo_t *info, void *ptr) {
+    pid_t pid = info->si_pid;
+    if (pid != 0) {
+      int outFd = open(pipeName, O_WRONLY);
+      write(outFd, &cnt, sizeof(cnt));
+      sleep(1);
+      if (munmap(arr, length) == -1) {
+        printf("Error un-mmapping the file: %s\n", strerror(errno));
+        exit(0);
+      }
+      close(fd);
+      close(outFd);
+      unlink(pipeName);
+      finished = true;
+      exit(0);
+    }
+    return;
+  }
+  struct sigaction new_action;
+  memset(&new_action, 0, sizeof(new_action));
+  new_action.sa_sigaction = signalHandler;
+  new_action.sa_flags = SA_SIGINFO;
+  if (0 != sigaction(SIGUSR2, &new_action, NULL)) {
+    printf(
+        "Signal handle registration "
+        "failed. %s\n",
+        strerror(errno));
+    return -1;
+  }
+
+  pid_t dispatcherPid = getppid();
+
+  while (!finished) {
+    int killStat = kill(dispatcherPid, SIGUSR1);
+  }
+  exit(0);
 }
--- ex3/dispatcher.c
+++ ex3/dispatcher.c
@@ -1,159 +1,138 @@
-#include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <math.h>
 #include <memory.h>
+#include <signal.h>
 #include <stdio.h>
 #include <sys/param.h>
-#include <signal.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <math.h>
-#include <fcntl.h>
+#include <unistd.h>
 
-//CREDIT hw2
-off_t getFileSize(const char *filename)
-{
-    struct stat st;
-    if (stat(filename, &st) == 0)
-        return st.st_size;
-    fprintf(stderr, "Cannot determine size of %s: %s\n",
-            filename, strerror(errno));
-    return -1;
+// CREDIT hw2
+off_t getFileSize(const char *filename) {
+  struct stat st;
+  if (stat(filename, &st) == 0) return st.st_size;
+  fprintf(stderr, "Cannot determine size of %s: %s\n", filename,
+          strerror(errno));
+  return -1;
 }
 
 size_t res = 0;
 int numOfForks = 1;
 
-pid_t forkLogic(char *targetChar, char *fileName, off_t offset, size_t length, int numOfForks)
-{
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        printf("fork failed: %s\n", strerror(errno));
-        return -1;
-    }
-    if (pid == 0)
-    {
-        char *args[7];
-        char offsetStr[1024];
-        char sizeStr[1024];
-        args[0] = "./counter";
-        args[1] = targetChar;
-        args[2] = fileName;
-        sprintf(offsetStr, "%jd", offset);
-        args[3] = offsetStr;
-        sprintf(sizeStr, "%zu", length);
-        args[4] = sizeStr;
-        args[5] = NULL;
-        execv("./counter", args);
-        printf("execv failed: %s\n", strerror(errno));
-        return -1;
-    }
-    return pid;
+pid_t forkLogic(char *targetChar, char *fileName, off_t offset, size_t length,
+                int numOfForks) {
+  pid_t pid = fork();
+  if (pid < 0) {
+    printf("fork failed: %s\n", strerror(errno));
+    return -1;
+  }
+  if (pid == 0) {
+    char *args[7];
+    char offsetStr[1024];
+    char sizeStr[1024];
+    args[0] = "./counter";
+    args[1] = targetChar;
+    args[2] = fileName;
+    sprintf(offsetStr, "%jd", offset);
+    args[3] = offsetStr;
+    sprintf(sizeStr, "%zu", length);
+    args[4] = sizeStr;
+    args[5] = NULL;
+    execv("./counter", args);
+    printf("execv failed: %s\n", strerror(errno));
+    return -1;
+  }
+  return pid;
 }
 
-off_t getPageSizeOffset(off_t originalOffset)
-{
-    off_t tmpOffset = originalOffset;
-    off_t pageSizeOffset = getpagesize();
-    off_t res = pageSizeOffset;
-    int i = 1;
-    while (tmpOffset > pageSizeOffset)
-    {
-        res = pageSizeOffset;
-        pageSizeOffset = pageSizeOffset * i;
-    }
-    return res;
+off_t getPageSizeOffset(off_t originalOffset) {
+  off_t tmpOffset = originalOffset;
+  off_t pageSizeOffset = getpagesize();
+  off_t res = pageSizeOffset;
+  int i = 1;
+  while (tmpOffset > pageSizeOffset) {
+    res = pageSizeOffset;
+    pageSizeOffset = pageSizeOffset * i;
+  }
+  return res;
 };
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+  char fileName[256];
+  if (argc < 3) {
+    printf("No enough arguments\n");
+    return -1;
+  }
+  strcpy(fileName, argv[2]);
+  size_t fileSize = (size_t)getFileSize(fileName);
+  double tmpM = sqrt(fileSize);
+  size_t m = (size_t)floor(tmpM);
+  pid_t pidArr[16];
+  int pidIndex = 0;
+  // CREDIT: register signal handler - Appendix A
 
-    char fileName[256];
-    if (argc < 3)
-    {
-        printf("No enough arguments\n");
-        return -1;
-    }
-    strcpy(fileName, argv[2]);
-    size_t fileSize = (size_t)getFileSize(fileName);
-    double tmpM = sqrt(fileSize);
-    size_t m = (size_t)floor(tmpM);
-    pid_t pidArr[16];
-    int pidIndex = 0;
-    // CREDIT: register signal handler - Appendix A
-
-    void signalHandler(int signum, siginfo_t *info, void *ptr)
-    {
-        pid_t pid = info->si_pid;
-        size_t toAdd;
-        char pipeName[256] = "/tmp/counter_";
-        sprintf(pipeName, "%s%d", pipeName, pid);
-        if (pid != 0)
-        {
-            int k;
-            for (k = 0; k < pidIndex; k++)
-            {
-                if (pidArr[k] == pid)
-                {
-                    return;
-                }
-            }
-            pidArr[pidIndex] = pid;
-            pidIndex++;
-            kill(pid, SIGUSR2);
-            int fd = open(pipeName, O_RDONLY);
-            if (fd < 0)
-            {
-                printf("Error opening pipe: %s\n", strerror(errno));
-                return;
-            }
-            ssize_t readBytes = read(fd, &toAdd, sizeof(size_t));
-            if (readBytes < 0)
-            {
-                printf("cannot read from pipe: %s\n", strerror(errno));
-                return;
-            }
-            res += toAdd;
+  void signalHandler(int signum, siginfo_t *info, void *ptr) {
+    pid_t pid = info->si_pid;
+    size_t toAdd;
+    char pipeName[256] = "/tmp/counter_";
+    sprintf(pipeName, "%s%d", pipeName, pid);
+    if (pid != 0) {
+      int k;
+      for (k = 0; k < pidIndex; k++) {
+        if (pidArr[k] == pid) {
+          return;
         }
+      }
+      pidArr[pidIndex] = pid;
+      pidIndex++;
+      kill(pid, SIGUSR2);
+      int fd = open(pipeName, O_RDONLY);
+      if (fd < 0) {
+        printf("Error opening pipe: %s\n", strerror(errno));
         return;
+      }
+      ssize_t readBytes = read(fd, &toAdd, sizeof(size_t));
+      if (readBytes < 0) {
+        printf("cannot read from pipe: %s\n", strerror(errno));
+        return;
+      }
+      res += toAdd;
     }
+    return;
+  }
 
-    struct sigaction new_action;
-    memset(&new_action, 0, sizeof(new_action));
-    new_action.sa_sigaction = signalHandler;
-    new_action.sa_flags = SA_SIGINFO;
-    if (0 != sigaction(SIGUSR1, &new_action, NULL))
-    {
-        printf("Signal handle registration "
-               "failed. %s\n",
-               strerror(errno));
-        return -1;
+  struct sigaction new_action;
+  memset(&new_action, 0, sizeof(new_action));
+  new_action.sa_sigaction = signalHandler;
+  new_action.sa_flags = SA_SIGINFO;
+  if (0 != sigaction(SIGUSR1, &new_action, NULL)) {
+    printf(
+        "Signal handle registration "
+        "failed. %s\n",
+        strerror(errno));
+    return -1;
+  }
+  off_t offset = 0;
+  if (m < 0) {
+    forkLogic(argv[1], argv[2], offset, fileSize, 1);
+  } else {
+    size_t chunkSize = (size_t)getPageSizeOffset((off_t)floor(fileSize / m));
+    while (offset < fileSize) {
+      forkLogic(argv[1], argv[2], offset, chunkSize, numOfForks);
+      numOfForks++;
+      offset += chunkSize;
     }
-    off_t offset = 0;
-    if (m < 0)
-    {
-        forkLogic(argv[1], argv[2], offset, fileSize, 1);
+    if (offset < fileSize) {
+      size_t delta = fileSize - offset;
+      forkLogic(argv[1], argv[2], offset, delta, numOfForks);
     }
-    else
-    {
-        size_t chunkSize = (size_t)getPageSizeOffset((off_t)floor(fileSize / m));
-        while (offset < fileSize)
-        {
-            forkLogic(argv[1], argv[2], offset, chunkSize, numOfForks);
-            numOfForks++;
-            offset += chunkSize;
-        }
-        if (offset < fileSize)
-        {
-            size_t delta = fileSize - offset;
-            forkLogic(argv[1], argv[2], offset, delta, numOfForks);
-        }
-    }
-    int j;
-    for (j = 0; j < numOfForks; j++)
-    {
-        wait(NULL);
-    }
-    printf("result: %zu\n", res);
-    return 0;
+  }
+  int j;
+  for (j = 0; j < numOfForks; j++) {
+    wait(NULL);
+  }
+  printf("result: %zu\n", res);
+  return 0;
 }
