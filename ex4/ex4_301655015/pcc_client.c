264dd2e60284ae25eee1d9e54615d7157578e89f
Dor Duchovni <dor@falcore.co>
2017-06-23 15:42:43 +0300

    changes

---
+++ ex4/ex4_301655015/pcc_client.c
@@ -0,0 +1,120 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Invalid arguments\n");
+        return 1;
+    }
+    size_t len;
+    sscanf(argv[1], "%zu", &len);
+
+
+
+    //CREDIT : recitation
+    int socketFd = -1;
+    ssize_t bytesRead = 0;
+    char inBuffer[1024];
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in my_addr;
+    struct sockaddr_in peer_addr;
+    socklen_t addrsize = sizeof(struct sockaddr_in);
+
+    memset(inBuffer, '0', sizeof(inBuffer));
+    if ((socketFd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    }
+
+    getsockname(socketFd, (struct sockaddr *) &my_addr, &addrsize);
+    printf("Client: socket created %s:%d\n",
+           inet_ntoa((my_addr.sin_addr)),
+           ntohs(my_addr.sin_port));
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(2233);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    printf("Client: connecting...\n");
+    if (connect(socketFd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\n Error : Connect Failed. %s \n", strerror(errno));
+        if (close(socketFd) == -1) {
+            printf("\n Error while closing fd %s \n", strerror(errno));
+        };
+        return 1;
+    }
+
+    if (write(socketFd, &len, sizeof(len)) == -1) {
+        printf("\n Error : cant write to socket %s \n", strerror(errno));
+        if (close(socketFd) == -1) {
+            printf("\n Error while closing fd %s \n", strerror(errno));
+        };
+        return 1;
+    }
+
+    int outFd = open("/dev/urandom", O_RDONLY);
+    if (outFd < 0) {
+        printf("\n Error : can't open /dev/urandom. %s \n", strerror(errno));
+        if (close(socketFd) == -1) {
+            printf("\n Error while closing fd %s \n", strerror(errno));
+        };
+        return 1;
+    }
+
+    size_t totalSent = 0;
+    while (totalSent < len) {
+        size_t toRead = totalSent + sizeof(inBuffer) < len ? sizeof(inBuffer) : len - totalSent;
+        ssize_t readBytes = read(outFd, inBuffer, toRead);
+        if (readBytes < 0) {
+            printf("\n Error while reading from /dev/urandom. %s \n", strerror(errno));
+            if (close(outFd) == -1) {
+                printf("\n Error while closing fd %s \n", strerror(errno));
+            };
+            if (close(socketFd) == -1) {
+                printf("\n Error while closing fd %s \n", strerror(errno));
+            };
+            return 1;
+        }
+        ssize_t writtenBytes = write(socketFd, inBuffer, (size_t) readBytes);
+        if (writtenBytes < 0) {
+            printf("\n Error while writing to socket %s \n", strerror(errno));
+            if (close(outFd) == -1) {
+                printf("\n Error while closing fd %s \n", strerror(errno));
+            };
+            if (close(socketFd) == -1) {
+                printf("\n Error while closing fd %s \n", strerror(errno));
+            };
+            return 1;
+        }
+        totalSent += writtenBytes;
+    }
+    size_t numOfPrintable;
+
+    while (1) {
+        bytesRead = read(socketFd, &numOfPrintable, sizeof(numOfPrintable));
+        if (bytesRead <= 0) {
+            break;
+        }
+    }
+    printf("\n result: %zu\n", numOfPrintable);
+    if (close(outFd) == -1) {
+        printf("\n Error while closing fd %s \n", strerror(errno));
+    };
+    if (close(socketFd) == -1) {
+        printf("\n Error while closing fd %s \n", strerror(errno));
+    };
+
+    exit(0);
+}
--- ex5/message_slot.c
+++ ex5/message_slot.c
@@ -1,18 +1,18 @@
 /* Declare what kind of code we want from the header files
-   Defining __KERNEL__ and MODULE allows us to access kernel-level 
+   Defining __KERNEL__ and MODULE allows us to access kernel-level
    code not usually available to userspace programs. */
 #undef __KERNEL__
 #define __KERNEL__ /* We're part of the kernel */
 #undef MODULE
-#define MODULE     /* Not a permanent part, though. */
+#define MODULE /* Not a permanent part, though. */
 
 /* ***** Example w/ minimal error handling - for ease of reading ***** */
 
-#include <linux/kernel.h>   /* We're doing kernel work */
-#include <linux/module.h>   /* Specifically, a module */
-#include <linux/fs.h>       /* for register_chrdev */
-#include <asm/uaccess.h>    /* for get_user and put_user */
-#include <linux/string.h>   /* for memset. NOTE - not string.h!*/
+#include <asm/uaccess.h>  /* for get_user and put_user */
+#include <linux/fs.h>     /* for register_chrdev */
+#include <linux/kernel.h> /* We're doing kernel work */
+#include <linux/module.h> /* Specifically, a module */
+#include <linux/string.h> /* for memset. NOTE - not string.h!*/
 
 MODULE_LICENSE("GPL");
 
@@ -21,79 +21,137 @@ MODULE_LICENSE("GPL");
 #define BUF_LEN 80
 #define DEVICE_FILE_NAME "message_slot"
 
-struct chardev_info{
-    spinlock_t lock;
+struct chardev_info {
+  spinlock_t lock;
 };
 
-static int dev_open_flag = 0; /* used to prevent concurent access into the same device */
-static struct chardev_info device_info;
+struct message_slot {
+  char **channels;
+};
 
+struct message_slot_list_node {
+  message_slot *message_slot1;
+  message_slot_list_node *next;
+  //   message_slot_list_node *prev;
+  char *id;
+  int current_index;
+};
+
+static struct message_slot_list_node first_node;
+static int dev_open_flag =
+    0; /* used to prevent concurent access into the same device */
+static struct chardev_info device_info;
 static char Message[BUF_LEN]; /* The message the device will give when asked */
-static int major; /* device major number */
+static int major;             /* device major number */
 
 /***************** char device functions *********************/
 
 /* process attempts to open the device file */
-static int device_open(struct inode *inode, struct file *file)
-{
-    unsigned long flags; // for spinlock
-    printk("device_open(%p)\n", file);
+static int device_open(struct inode *inode, struct file *file) {
+  unsigned long flags;  // for spinlock
+  printk("device_open(%p)\n", file);
 
-    /* 
-     * We don't want to talk to two processes at the same time 
-     */
-    spin_lock_irqsave(&device_info.lock, flags);
-    if (dev_open_flag){
-        spin_unlock_irqrestore(&device_info.lock, flags);
-        return -EBUSY;
-    }
-
-    dev_open_flag++;
-    spin_unlock_irqrestore(&device_info.lock, flags); 
-
-    return SUCCESS;
-}
-
-static int device_release(struct inode *inode, struct file *file)
-{
-    unsigned long flags; // for spinlock
-    printk("device_release(%p,%p)\n", inode, file);
-
-    /* ready for our next caller */
-    spin_lock_irqsave(&device_info.lock, flags);
-    dev_open_flag--;
+  /*
+   * We don't want to talk to two processes at the same time
+   */
+  spin_lock_irqsave(&device_info.lock, flags);
+  if (dev_open_flag) {
     spin_unlock_irqrestore(&device_info.lock, flags);
+    return -EBUSY;
+  }
 
-    return SUCCESS;
+  dev_open_flag++;
+  struct message_slot_list_node *current_list_node = first_node;
+  while (current_list_node->next != NULL &&
+         strcmp(current_list_node->id, file->f_inode->i_ino) != 0) {
+    current_list_node = current_list_node->next;
+  }
+  if (strcmp(current_list_node->id, file->f_inode->i_ino) != 0)) {
+      // current is good
+    }
+  else {
+    current_list_node->next =
+        (message_slot_list_node *)kmalloc(sizeof(message_slot_list_node));
+    current_list_node = current_list_node->next;
+    current_list_node->id = file->f_inode->i_ino;
+    current_list_node->next = NULL;
+    current_list_node->message_slot1 =
+        (*message_slot)kmalloc(sizeof(message_slot));
+    message_slot message_slot1 = current_list_node->message_slot1;
+    int i;
+    for (i = 0; i < 4; i++) {
+      message_slot1[i] = (char *)kmalloc(128);
+    }
+  }
+
+  spin_unlock_irqrestore(&device_info.lock, flags);
+
+  return SUCCESS;
 }
 
-/* a process which has already opened 
+static int device_release(struct inode *inode, struct file *file) {
+  unsigned long flags;  // for spinlock
+  printk("device_release(%p,%p)\n", inode, file);
+
+  /* ready for our next caller */
+  spin_lock_irqsave(&device_info.lock, flags);
+  dev_open_flag--;
+  spin_unlock_irqrestore(&device_info.lock, flags);
+
+  return SUCCESS;
+}
+
+/* a process which has already opened
    the device file attempts to read from it */
 static ssize_t device_read(struct file *file, /* see include/linux/fs.h   */
-               char __user * buffer, /* buffer to be filled with data */
-               size_t length,  /* length of the buffer     */
-               loff_t * offset)
-{
-    /* read doesnt really do anything (for now) */
-    printk("device_read(%p,%d) - operation not supported yet (last written - %s)\n", file, length, Message);
+                           char __user
 
-    return -EINVAL; // invalid argument error
+                               *buffer,   /* buffer to be filled with data */
+                           size_t length, /* length of the buffer     */
+                           loff_t *offset) {
+  /* read doesnt really do anything (for now) */
+  printk(
+      "device_read(%p,%d) - operation not supported yet (last written - %s)\n",
+      file, length, Message);
+
+  return -EINVAL;  // invalid argument error
+}
+
+static long device_ioctl(                      // struct inode*  inode,
+    struct file *file, unsigned int ioctl_num, /* The number of the ioctl */
+    unsigned long ioctl_param)                 /* The parameter to it */
+{
+
+  /* Switch according to the ioctl called */
+  if (IOCTL_SET_ENC == ioctl_num) {
+      int index = (int)*ioctl_param;
+      if (index < 0 || index > 3) {
+            // todo bom
+          return -1;
+      }
+
+    /* Get the parameter given to ioctl by the process */
+    if (ioctl_param < 0 ||)
+    printk("chardev, ioctl: setting encryption flag to %ld\n", ioctl_param);
+    encryption_flag = ioctl_param;
+  }
+
+  return SUCCESS;
 }
 
 /* somebody tries to write into our device file */
-static ssize_t
-device_write(struct file *file,
-         const char __user * buffer, size_t length, loff_t * offset)
-{
-    int i;
+static ssize_t device_write(struct file *file, const char __user
 
-    printk("device_write(%p,%d)\n", file, length);
+                                                   *buffer,
+                            size_t length, loff_t *offset) {
+  int i;
 
-    for (i = 0; i < length && i < BUF_LEN; i++)
-        get_user(Message[i], buffer + i);
- 
-    /* return the number of input characters used */
-    return i;
+  printk("device_write(%p,%d)\n", file, length);
+
+  for (i = 0; i < length && i < BUF_LEN; i++) get_user(Message[i], buffer + i);
+
+  /* return the number of input characters used */
+  return i;
 }
 
 /************** Module Declarations *****************/
@@ -102,50 +160,54 @@ device_write(struct file *file,
  * when a process does something to the device we created */
 struct file_operations Fops = {
     .open = device_open,
-    .ioctl = device_ioctl,
+    .unlocked_ioctl = device_ioctl,
     .read = device_read,
-    .write = device_write,    /* a.k.a. close */
-    .release = device_release,    /* a.k.a. close */
+    .write = device_write,     /* a.k.a. close */
+    .release = device_release, /* a.k.a. close */
 };
 
-/* Called when module is loaded. 
+/* Called when module is loaded.
  * Initialize the module - Register the character device */
-static int __init init(void)
-{
-    /* init dev struct*/
-    memset(&device_info, 0, sizeof(struct chardev_info));
-    spin_lock_init(&device_info.lock);    
+static int __init init(void) {
+  /* init dev struct*/
+  memset(&device_info, 0, sizeof(struct chardev_info));
+  spin_lock_init(&device_info.lock);
 
-    /* Register a character device. Get newly assigned major num */
-    major = register_chrdev(0, DEVICE_RANGE_NAME, &Fops /* our own file operations struct */);
+  /* Register a character device. Get newly assigned major num */
+  major = register_chrdev(0, DEVICE_RANGE_NAME,
+                          &Fops /* our own file operations struct */);
 
-    /* 
-     * Negative values signify an error 
-     */
-    if (major < 0) {
-        printk(KERN_ALERT "%s failed with %d\n",
-               "Sorry, registering the character device ", major);
-        return major;
-    }
+  /*
+   * Negative values signify an error
+   */
+  if (major < 0) {
+    printk(KERN_ALERT "%s failed with %d\n",
+           "Sorry, registering the character device ", major);
+    return major;
+  }
+  first_node.next = NULL;
+  //   first_node.prev = NULL;
+  first_node.message_slot = NULL;
+  printk("Registeration is a success. The major device number is %d.\n", major);
+  printk("If you want to talk to the device driver,\n");
+  printk("you have to create a device file:\n");
+  printk("mknod /dev/%s c %d 0\n", DEVICE_FILE_NAME, major);
+  printk("You can echo/cat to/from the device file.\n");
+  printk("Dont forget to rm the device file and rmmod when you're done\n");
 
-    printk("Registeration is a success. The major device number is %d.\n", major);
-    printk("If you want to talk to the device driver,\n");
-    printk("you have to create a device file:\n");
-    printk("mknod /dev/%s c %d 0\n", DEVICE_FILE_NAME, major);
-    printk("You can echo/cat to/from the device file.\n");
-    printk("Dont forget to rm the device file and rmmod when you're done\n");
-
-    return 0;
+  return 0;
 }
 
 /* Cleanup - unregister the appropriate file from /proc */
-static void __exit cleanup(void)
-{
-    /* 
-     * Unregister the device 
-     * should always succeed (didnt used to in older kernel versions)
-     */
-    unregister_chrdev(major, DEVICE_RANGE_NAME);
+static void __exit cleanup(void) {
+  /*
+   * Unregister the device
+   * should always succeed (didnt used to in older kernel versions)
+   */
+
+  // free them all
+
+  unregister_chrdev(major, DEVICE_RANGE_NAME);
 }
 
 module_init(init);
